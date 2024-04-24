9efc8f3c6229d578e48bbcbe0bdb7e663ac5dd0c
BarbaraZamperete <bahzamp25@gmail.com>
2024-04-24 09:37:07 -0400

    refatoração

--- dogia_app/signals.py
+++ dogia_app/signals.py
@@ -8,6 +8,34 @@ from .models import Imagem, Combinacao, Cachorro
 from .ia.embedding_creation import embedding_creation
 from .ia.score_creation import distance_cal, score_creation
 
+def calc_min_distancia():
+    return Combinacao.objects.all().aggregate(Min('distancia_bruta'))['distancia_bruta__min'] or 0
+
+def calc_max_distancia():
+    return Combinacao.objects.all().aggregate(Max('distancia_bruta'))['distancia_bruta__max'] or 1
+
+def calc_distancia(distancia_bruta ):
+    max_distancia = calc_max_distancia()
+    min_distancia = calc_min_distancia()
+    fator_escala = 0.2
+
+    return (1 - fator_escala * (distancia_bruta - min_distancia) / (max_distancia - min_distancia))
+
+def calc_score(score_bruto):
+    soma_score_bruto = Combinacao.objects.aggregate(soma=Sum('score_bruto'))['soma']
+    if soma_score_bruto != 0:
+        score_normalizado = score_bruto/soma_score_bruto
+    else:
+        score_normalizado = score_bruto
+    return score_normalizado
+
+def update_combinacoes(id):
+    outras_combinacoes = Combinacao.objects.exclude(pk=id)
+    for outra_combinacao in outras_combinacoes:
+        outra_combinacao.distancia = calc_distancia(outra_combinacao.distancia_bruta)
+        outra_combinacao.score = calc_score(outra_combinacao.score_bruto)
+        outra_combinacao.save()
+
 @receiver(post_save, sender=Imagem)
 def process_image(sender, instance, created, **kwargs):
     if created and not instance.embedding:
@@ -40,28 +68,16 @@ def process_image(sender, instance, created, **kwargs):
                 distancia_bruta = distance_cal(emb_1, emb_2)
                 comb.distancia_bruta = distancia_bruta
 
-                # Obtém o fator de escala usando o Max e Min das distâncias já existentes
-                max_distancia = Combinacao.objects.all().aggregate(Max('distancia_bruta'))['distancia_bruta__max'] or 1
-                min_distancia = Combinacao.objects.all().aggregate(Min('distancia_bruta'))['distancia_bruta__min'] or 0
-                fator_escala = 0.2
+                comb.distancia = calc_distancia(distancia_bruta)
 
-                distancia = 1 - fator_escala * (distancia_bruta - min_distancia) / (max_distancia - min_distancia)
-                comb.distancia = distancia
-
-                score_bruto = score_creation(instance.cachorro, img_1.cachorro, distancia)
+                score_bruto = score_creation(instance.cachorro, img_1.cachorro, comb.distancia)
                 comb.score_bruto = score_bruto  
 
-                soma_score_bruto = Combinacao.objects.aggregate(soma=Sum('score_bruto'))['soma']
-                score_normalizado = score_bruto/soma_score_bruto
-                comb.score = score_normalizado
+                comb.score = calc_score(score_bruto)
 
                 comb.save()
 
-                outras_combinacoes = Combinacao.objects.exclude(pk=comb.pk)
-                for outra_combinacao in outras_combinacoes:
-                    outra_combinacao.distancia = 1 - fator_escala * (outra_combinacao.distancia_bruta - min_distancia) / (max_distancia - min_distancia)
-                    outra_combinacao.score = outra_combinacao.score_bruto/soma_score_bruto
-                    outra_combinacao.save()
+                update_combinacoes(comb.id)
 
         
         if (instance.cachorro.tipo == 2):
@@ -87,80 +103,12 @@ def process_image(sender, instance, created, **kwargs):
                 distancia_bruta = distance_cal(emb_1, emb_2)
                 comb.distancia_bruta = distancia_bruta
 
-                # Obtém o fator de escala usando o Max e Min das distâncias já existentes
-                max_distancia = Combinacao.objects.all().aggregate(Max('distancia_bruta'))['distancia_bruta__max'] or 1
-                min_distancia = Combinacao.objects.all().aggregate(Min('distancia_bruta'))['distancia_bruta__min'] or 0
-                fator_escala = 0.2
+                comb.distancia = calc_distancia(distancia_bruta)
 
-                distancia = 1 - fator_escala * (distancia_bruta - min_distancia) / (max_distancia - min_distancia)
-                comb.distancia = distancia
-
-                score_bruto = score_creation(img_1.cachorro, instance.cachorro, distancia)
+                score_bruto = score_creation(img_1.cachorro, instance.cachorro, comb.distancia)
                 comb.score_bruto = score_bruto  
 
-                soma_score_bruto = Combinacao.objects.aggregate(soma=Sum('score_bruto'))['soma']
-                print(soma_score_bruto)
-                print(score_bruto)
-
-                if soma_score_bruto != 0:
-                    score_normalizado = score_bruto/soma_score_bruto
-                else:
-                    score_normalizado = score_bruto
-                
-                print(soma_score_bruto)
-                print(score_bruto)
-
-                comb.score = score_normalizado
+                comb.score = calc_score(score_bruto)
 
                 comb.save()
-
-                outras_combinacoes = Combinacao.objects.exclude(pk=comb.pk)
-                for outra_combinacao in outras_combinacoes:
-                    outra_combinacao.distancia = 1 - fator_escala * (outra_combinacao.distancia_bruta - min_distancia) / (max_distancia - min_distancia)
-                    outra_combinacao.score = outra_combinacao.score_bruto/soma_score_bruto
-                    outra_combinacao.save()
-
-
-
-# @receiver(post_save, sender=Combinacao)
-# def criar_combinacoes(sender, instance, created, **kwargs):
-#     if created:
-#         cachorro_buscado = instance.id_buscado
-#         cachorro_avistado = instance.id_avistado
-
-#         img_buscado = Imagem.objects.get(cachorro=cachorro_buscado)
-#         img_avistado = Imagem.objects.get(cachorro=cachorro_avistado)
-
-#         embedding_buscado = img_buscado.embedding
-#         embedding_avistado = img_avistado.embedding
-
-#         distancia_bruta = distance_cal(embedding_buscado, embedding_avistado)
-#         instance.distancia_bruta = distancia_bruta
-#         # Obtém o fator de escala usando o Max e Min das distâncias já existentes
-#         max_distancia = Combinacao.objects.all().aggregate(Max('distancia_bruta'))['distancia_bruta__max'] or 1
-#         min_distancia = Combinacao.objects.all().aggregate(Min('distancia_bruta'))['distancia_bruta__min'] or 0
-#         # fator_escala = 1.0 / (max_distancia - min_distancia) if max_distancia != min_distancia else 1.0
-#         fator_escala = 0.2
-
-#         # Normaliza a distância da nova combinação
-#         distancia = 1 - fator_escala * (distancia_bruta - min_distancia) / (max_distancia - min_distancia)
-#         instance.distancia = distancia
-
-#         # Normaliza o score da nova combinação
-#         score_bruto = score_creation(cachorro_buscado, cachorro_avistado, distancia)
-#         instance.score_bruto = score_bruto
-
-#         soma_score_bruto = Combinacao.objects.aggregate(soma=Sum('score_bruto'))['soma']
-#         score_normalizado = score_bruto/soma_score_bruto
-#         instance.score = score_normalizado
-
-#         instance.save()
-
-#         # Atualiza as distâncias normalizadas das outras instâncias já salvas
-#         outras_combinacoes = Combinacao.objects.exclude(pk=instance.pk)
-#         for outra_combinacao in outras_combinacoes:
-#             outra_combinacao.distancia = 1 - fator_escala * (outra_combinacao.distancia_bruta - min_distancia) / (max_distancia - min_distancia)
-#             outra_combinacao.score = outra_combinacao.score_bruto/soma_score_bruto
-#             outra_combinacao.save()
-
-        
+                update_combinacoes(comb.id)
