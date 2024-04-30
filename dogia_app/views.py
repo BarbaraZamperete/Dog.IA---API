1ebd4cc5e5fe0c196cfc7b8777855e19099458e1
BarbaraZamperete <bahzamp25@gmail.com>
2024-04-30 09:14:32 -0400

    correção na forma como um usuário é criado - criptografa a senha

--- dogia_app/views.py
+++ dogia_app/views.py
@@ -10,6 +10,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.authentication import SessionAuthentication
 from rest_framework import viewsets
+from django.contrib.auth.hashers import make_password
 
 
 ###################################################################################
@@ -26,6 +27,7 @@ class UserViewSet(viewsets.ModelViewSet):
         return super().get_permissions()
 
     def create(self, request, *args, **kwargs):
+        request.data['password'] = make_password(request.data['password'])
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
@@ -71,14 +73,13 @@ class UserLogIn(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
-        username = serializer.validated_data['username']
-        usuario = Usuario.objects.get(username=username)
-        token = Token.objects.get(user=usuario)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
         return Response({
             'token': token.key,
-            'id': usuario.pk,
-            'username': usuario.username
-        })
+            'id': user.pk,
+            'username': user.username
+        }, status=status.HTTP_200_OK)
 
 
 
